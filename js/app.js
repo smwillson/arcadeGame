2e0f7b8e04e9fed01e1add175dd3269fe860d927
Sama Mushtaq  <sm9wani@gmail.com>
2018-08-31 18:49:48 -0500

    Collision implemented correctly

--- js/app.js
+++ js/app.js
@@ -4,24 +4,22 @@ var hasPlayerExecuted = false;
 const totalEnemySprites = 5;
 const startingXPosition = -100;
 const playerStartXPosition = 200,
-    playerStartYPosition = 200;
+	playerStartYPosition = 200;
 var allowedKeys;
-var now = Date.now();
-var totalTimeElasped, totalEmenySprites;
 var isEnemyOutOfBounds, isPlayerOutOfBounds;
 var isCollision = false;
 const spriteMovement = 15;
 
-var Enemy = function(yCoordinate) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+var Enemy = function (yCoordinate) {
+	// Variables applied to each of our instances go here,
+	// we've provided one for you to get started
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+	// The image/sprite for our enemies, this uses
+	// a helper we've provided to easily load images
 
-    this.sprite = 'images/enemy-bug.png';
-    this.x = startingXPosition;
-    this.y = yCoordinate;
+	this.sprite = 'images/enemy-bug.png';
+	this.x = startingXPosition;
+	this.y = yCoordinate;
 
 
 };
@@ -29,46 +27,59 @@ var Enemy = function(yCoordinate) {
 //give the enemy sprite a speed at which it moves; speed will be between x and 100x.
 //random number generator : https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript
 Enemy.prototype.rateOfMovement = function randomIntFromInterval(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
+	return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+Enemy.prototype.update = function (dt) {
+	// You should multiply any movement by the dt parameter
+	// which will ensure the game runs at the same speed for
+	// all computers.
+	if (!isCollision) {
+		if (!this.checkIfOutOfBoundsEnemy(this)) {
+			this.x += (dt * this.rateOfMovement(1, 10));
+		} else {
+			this.x = startingXPosition;
+			isEnemyOutOfBounds = false;
+		}
+    //detect collision
+    //https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection
+		if (player.x < this.x + this.width / 2 &&
+			player.x + player.width / 2 > this.x &&
+			player.y < this.y + this.height / 2 &&
+			player.y + player.height / 2 > this.y) {
+			isCollision = true;
+		}
 
-    if (!this.checkIfOutOfBoundsEnemy(this)) {
-        this.x += (dt * this.rateOfMovement(10, 1000));
-    } else {
-        this.x = startingXPosition;
-        isEnemyOutOfBounds = false;
-    }
+	} else {
+
+
+		player.reset();
+	}
 };
 
 
 // Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+Enemy.prototype.render = function () {
 
-    var sprite = Resources.get(this.sprite);
+	var sprite = Resources.get(this.sprite);
 
-    var height = sprite.height,
-        width = sprite.width;
+	var height = sprite.height,
+		width = sprite.width;
 
-    if (!hasExecuted) {
-        (function() {
+	if (!hasExecuted) {
+		(function () {
 
-            Enemy.prototype.height = height;
+			Enemy.prototype.height = height;
 
-            Enemy.prototype.width = width;
-            console.log('in function!');
+			Enemy.prototype.width = width;
 
-        })();
-        hasExecuted = true;
-    }
 
-    ctx.drawImage(sprite, this.x, this.y + height);
+		})();
+		hasExecuted = true;
+	}
+	ctx.drawImage(sprite, this.x, this.y + height);
 
 };
 
@@ -78,114 +89,117 @@ var allEnemies = [];
 
 (function createEnemySprites() {
 
-    let yCoordinates = [-130, -35, 60, 155, 250, 345];
+	let yCoordinates = [-130, -35, 60, 155, 250, 345];
 
-    for (let enemyCount = 0; enemyCount < totalEnemySprites; enemyCount++) {
+	for (let enemyCount = 0; enemyCount < totalEnemySprites; enemyCount++) {
 
-        allEnemies.push(new Enemy(yCoordinates[enemyCount]));
-    }
+		allEnemies.push(new Enemy(yCoordinates[enemyCount]));
+	}
 
 })();
 
 //check if enemy is outside the boundary of the canvas
-Enemy.prototype.checkIfOutOfBoundsEnemy = function(enemy) {
-    //  console.log('I am at'+enemy.x);
-    if (enemy.x > 505) {
+Enemy.prototype.checkIfOutOfBoundsEnemy = function (enemy) {
+	//  console.log('I am at'+enemy.x);
+	if (enemy.x > 505) {
 
-        isEnemyOutOfBounds = true;
+		isEnemyOutOfBounds = true;
 
-    }
-    return isEnemyOutOfBounds;
+	}
+	return isEnemyOutOfBounds;
 };
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 
-var Player = function() {
-    this.sprite = 'images/char-pink-girl.png';
-    this.x = playerStartXPosition;
-    this.y = playerStartYPosition;
+var Player = function () {
+	this.sprite = 'images/char-pink-girl.png';
+	this.x = playerStartXPosition;
+	this.y = playerStartYPosition;
 };
 //y =-180 when sprite touches water
-Player.prototype.update = function() {
+Player.prototype.update = function () {
 
-    if (!(this.checkCollisions())){
-    if (this.x > 420) {
-        this.x = this.x - spriteMovement;
-    }
-    if (this.x < -10) {
-        this.x = this.x + spriteMovement;
-    }
-    if (this.y > 280) {
-        this.y = this.y - spriteMovement;
-    }
-    if (this.y <= -180) {
-        window.alert('Game won!');
-        this.x = playerStartXPosition;
-        this.y = playerStartXPosition;
-    }
-  }
-  else this.reset();
+
+	if (this.x > 420) {
+		this.x = this.x - spriteMovement;
+	}
+	if (this.x < -10) {
+		this.x = this.x + spriteMovement;
+	}
+	if (this.y > 280) {
+		this.y = this.y - spriteMovement;
+	}
+	if (this.y <= -180) {
+		window.alert('Game won!');
+		this.x = playerStartXPosition;
+		this.y = playerStartXPosition;
+	}
 
 
 }
 
 //what to do when a collision occurs
 
-Player.prototype.reset = function(){
+Player.prototype.reset = function () {
 
-    this.x = playerStartXPosition;
-    this.y = playerStartYPosition;
-    isCollision = false;
+	this.x = playerStartXPosition;
+	this.y = playerStartYPosition;
+	isCollision = false;
 }
 
 // Draw the enemy on the screen, required method for game
-Player.prototype.render = function() {
+Player.prototype.render = function () {
 
-    let sprite = Resources.get(this.sprite);
+	let sprite = Resources.get(this.sprite);
 
-    var height = sprite.height,
-        width = sprite.width;
+	var height = sprite.height,
+		width = sprite.width;
 
-    if (!hasPlayerExecuted) {
-        (function() {
+	if (!hasPlayerExecuted) {
+		(function () {
 
-            Player.prototype.height = height;
+			Player.prototype.height = height;
 
-            Player.prototype.width = width;
-            console.log('in function!');
+			Player.prototype.width = width;
 
-        })();
-        hasPlayerExecuted = true;
-    }
 
-    ctx.drawImage(sprite, this.x, this.y + height);
+		})();
+		hasPlayerExecuted = true;
+	}
+
+	ctx.drawImage(sprite, this.x, this.y + height);
 
 };
 
-Player.prototype.handleInput = function(allowedKeyCode) {
+Player.prototype.handleInput = function (allowedKeyCode) {
 
-    switch (allowedKeyCode) {
-        case 'left':
-            this.x -= spriteMovement;
+	switch (allowedKeyCode) {
+		case 'left':
 
-            break;
-        case 'right':
-            this.x += spriteMovement;
+			this.x -= spriteMovement;
 
-            break;
-        case 'up':
-            this.y -= spriteMovement;
 
-            break;
-        case 'down':
-            this.y += spriteMovement;
+			break;
+		case 'right':
 
-            break;
-        default:
+			this.x += spriteMovement;
 
-    }
+			break;
+		case 'up':
+
+			this.y -= spriteMovement;
+
+			break;
+		case 'down':
+
+			this.y += spriteMovement;
+
+			break;
+		default:
+
+	}
 
 };
 var player = new Player();
@@ -194,37 +208,15 @@ var player = new Player();
 // Place the player object in a variable called player
 
 
-//detect collision
-//https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection
-Player.prototype.checkCollisions = function() {
-
-    allEnemies.forEach(function(enemy) {
-
-        if (!(this.x < enemy.x + enemy.width &&
-                this.x + this.width > enemy.x &&
-                this.y < enemy.y + enemy.height &&
-                this.y + this.height > enemy.y)) {
-
-            isCollision = true;
-
-        }
-
-    });
-
-return isCollision;
-}
-
-
-
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
-    allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
+document.addEventListener('keyup', function (e) {
+	allowedKeys = {
+		37: 'left',
+		38: 'up',
+		39: 'right',
+		40: 'down'
+	};
 
-    player.handleInput(allowedKeys[e.keyCode]);
+	player.handleInput(allowedKeys[e.keyCode]);
 });
